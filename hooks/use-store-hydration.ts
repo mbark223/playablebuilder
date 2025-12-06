6fd1a2aa71e8ce37965b549377f356a9006dfef0
mbark223 <max@flatiron.co>
2025-12-05 22:15:23 -0500

    Fix hydration hook for client

--- hooks/use-store-hydration.ts
+++ hooks/use-store-hydration.ts
@@ -1,8 +1,28 @@
-import { useEffect } from 'react'
+'use client'
+
+import { useEffect, useState } from 'react'
 import { useProjectStore } from '@/store/project-store'
 
 export function useStoreHydration() {
+  const [hasHydrated, setHasHydrated] = useState(() => {
+    return typeof window === 'undefined'
+      ? false
+      : useProjectStore.persist.hasHydrated?.() ?? false
+  })
+
   useEffect(() => {
-    useProjectStore.persist.rehydrate()
+    const unsubscribe = useProjectStore.persist.onFinishHydration?.(() => {
+      setHasHydrated(true)
+    })
+
+    if (useProjectStore.persist.hasHydrated?.()) {
+      setHasHydrated(true)
+    }
+
+    return () => {
+      unsubscribe?.()
+    }
   }, [])
+
+  return hasHydrated
 }
